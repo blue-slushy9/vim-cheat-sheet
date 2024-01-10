21dedd5b6a2a48a1fc6e1cca4602fe19183c8582
blue-slushy9 <ra.cruz971@gmail.com>
2024-01-10 10:46:53 -0800

    added my first vim cheat

---
+++ vim_cheat_sheet.sh
@@ -0,0 +1,8 @@
+# BLOCK COMMENTS
+First, go to the first line you want to comment, press CtrlV. This will put the editor in the VISUAL BLOCK mode.
+
+Then use the arrow key or J/K and select until the last line
+
+Now press Shift+I, which will put the editor in INSERT mode and then press #. This will add a hash to the first line.
+    
+Then press Esc (give it a second), and it will insert a # character on all other selected lines.
