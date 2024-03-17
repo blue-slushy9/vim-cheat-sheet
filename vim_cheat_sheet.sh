c1eb8bb703dfc34fa72270a05f9aa747a0b7ab2e
blue-slushy9 <ra.cruz971@gmail.com>
2024-03-17 09:33:52 -0700

    added block indent and unindent instructions

--- vim_cheat_sheet.sh
+++ vim_cheat_sheet.sh
@@ -4,3 +4,12 @@
 -Now press Shift+I, which will put the editor in INSERT mode. 
 -Then press # (or whatever the correct comment symbol is), this will add the symbol to the first line.
 -Then press Esc (give it a second), and it will insert that same symbol on all other selected lines.
+
+# BLOCK UNINDENT
+-First, go to the first line you want to comment, press Ctrl+V. This will put the editor in the VISUAL BLOCK mode.
+-Then use the arrow keys or J/K and select up to the last line.
+-Type << (this is the key that is shared with the comma, not the arrows)
+-Press Escape to terminate the process, this will prevent any additional unintentional shifting
+
+# BLOCK INDENT
+-Same as above, except you type >> instead
